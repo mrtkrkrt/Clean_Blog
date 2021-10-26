f031bf53ceb669768d505dfd008dfb4dbc910626
mrtkrkrt <muratkrkrt0711@gmail.com>
2021-10-26 15:26:36 +0300

    setting up partials

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
 
 const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("HOŞGELDİNİZ!!!");
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/add_post", (req, res) => {
+  res.render("add_post");
 });
 
 app.listen(port, () => {
