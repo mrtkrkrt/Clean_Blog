0d694039062b078d32ee7152e8b6e32ff2b5f5d2
mrtkrkrt <muratkrkrt0711@gmail.com>
2021-10-30 18:07:05 +0300

    create database

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
-const express = require("express");
-const ejs = require("ejs");
-const path = require("path");
+const express = require('express');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+const path = require('path');
+const Post = require('./models/Post');
 
 const app = express();
-app.use(express.static("public"));
-app.set("view engine", "ejs");
+
+mongoose.connect('mongodb://localhost/Clean_Blog', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get('/', async (req, res) => {
+    //res.render('index');
+    const posts = await Post.find({});
+    res.render('index', {
+        posts,
+    });
 });
 
-app.get("/about", (req, res) => {
-  res.render("about");
+app.get('/about', (req, res) => {
+    res.render('about');
 });
 
-app.get("/add_post", (req, res) => {
-  res.render("add_post");
+app.get('/add_post', (req, res) => {
+    res.render('add_post');
+});
+
+app.post('/posts', async (req, res) => {
+    await Post.create(req.body);
+    res.redirect('/');
 });
 
 app.listen(port, () => {
-  console.log(`Sunucu ${port} portunda başlatıldı.`);
+    console.log(`Sunucu ${port} portunda başlatıldı.`);
 });
